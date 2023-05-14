80c1b895de8761ce8e2c45f5da3cbb036a0d563e
Sudeep Karki <sudeepkarki123@gmail.com>
2023-05-14 14:10:09 +0545

    load balancer

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "blog_vpc" {
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
- # enable_nat_gateway = true
+  enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
@@ -39,6 +39,48 @@ resource "aws_instance" "blog" {
 
   tags = {
     Name = "Learning Terraform"
+  }
+}
+
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.bog_sg.security_group_id
+
+  target_groups = [
+    {
+      name_prefix      = "blog"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = "aws_instance.blog.id"
+          port = 80
+        }
+      }
+    }
+  ]
+
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "Dev"
   }
 }
 
