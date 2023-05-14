751de5bcf88785b2844f9c97b3ec1c92fa30459d
Sudeep Karki <sudeepkarki123@gmail.com>
2023-05-14 14:35:09 +0545

    alb-and-asg

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "blog_vpc" {
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
- # enable_nat_gateway = true
+  enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
@@ -32,26 +32,24 @@ module "blog_vpc" {
 }
 
 
-module "autoscaling" {
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.9.0"
+  version = "6.5.2"
 
-  name     = "blog"
-  min_size = 1
-  max_size = 2
+  name = "blog"
 
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   =  module.blog_alb.target_group_arns
-  security_groups = [module.blog_sg.security_group_id]
-
-  image_id               = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+#  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"
+  version = "~> 6.0"
 
   name = "blog-alb"
 
@@ -63,19 +61,12 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "blog"
+      name_prefix      = "blog-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = "aws_instance.blog.id"
-          port = 80
-        }
-      }
     }
   ]
-
 
   http_tcp_listeners = [
     {
@@ -86,7 +77,7 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = "Dev"
+    Environment = "dev"
   }
 }
 
